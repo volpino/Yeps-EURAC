104121e6e7e5b3c44f312f676cdc0ea880d0b81e
fox <fox91@anche.no>
2010-01-03 19:25:18 +0100

    new tools

---
+++ tools/visualization/cluster_plot.py
@@ -0,0 +1,41 @@
+from numpy import *
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import csv
+import iodata
+
+from sys import argv
+
+try:
+    input1 = argv[1]
+    input2 = argv[2]
+    output = argv[3]
+except KeyError:
+    print "Invalid input!"
+    exit(1)
+
+r = csv.reader(open(input1), delimiter='\t')
+centroidsid = array(r[0])
+mini = array(r[1])
+
+x, mat, header, title = iodata.load_csv(input2, '\t')
+
+clusters = {}
+i = 0
+for c in mini:
+    try:
+        clusters[c].append(mat[i])
+    except KeyError:
+        clusters[c] = [mat[i]]
+    i += 1
+colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+i = 0
+for cluster in clusters.values():
+    i = i % len(colours)
+    for time_series in cluster:
+        line = plt.plot(time_series)
+        plt.setp(line, color=colours[i])
+    i += 1
+plt.ylabel(title)
+plt.savefig(output, format="png")
---
+++ tools/visualization/cluster_report.py
@@ -0,0 +1,37 @@
+from numpy import *
+import csv
+import iodata
+from sys import argv
+
+try:
+    input1 = argv[1]
+    input2 = argv[2]
+    output = argv[3]
+except KeyError:
+    print "Invalid input!"
+    exit(1)
+
+r = csv.reader(open(input1), delimiter='\t')
+centroidsid = array(r[0])
+mini = array(r[1])
+
+x, mat, header, title = iodata.load_csv(input2, '\t')
+
+centroid_matrix = zeros((matt.shape[0], centroidsid.shape[0]))
+for i in range(centroidsid.shape[0]):
+    centroid_matrix[:, i] = matt[:, centroidsid[i]-1]
+
+f=open(output,"w")
+writer = csv.writer(f, delimiter='\t', lineterminator='\n')
+
+f.write("Call: iteration %s, distance %s, fast %s, radius %d, seed %s, tollerance %f\n" % (options.nrip,options.met, options.fast,options.radius,options.seed,options.error))
+f.write("centroid.matrix\n")
+writer.writerows(centroid_matrix)
+
+f.write("\ncentroid.idx\n")
+writer.writerow(centroidsid)
+f.write("\ngroups\n")
+
+for i in range(mini.shape[0]):
+    writer.writerow([i+1, int(mini[i])])
+f.close()
