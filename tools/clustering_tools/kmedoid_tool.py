e182af24be42d2432afdd956bb6d6a9ae45ccc4c
fox <fox91@anche.no>
2010-01-03 17:56:49 +0100

    kmedoid plot tool

--- tools/clustering_tools/kmedoid_tool.py
+++ tools/clustering_tools/kmedoid_tool.py
@@ -6,7 +6,6 @@ import kmedoid
 import iodata
 import csv
 
-
 # Command line parsin
 parser = OptionParser()
 parser.add_option("-d", "--data", metavar = "CSV",
@@ -29,7 +28,8 @@ parser.add_option("-t", "--tolerance", type = "float",
                   dest = "error", help = "tolerance of algorithm, default = 0.0001", default=0.0001)
 parser.add_option("-o", "--output", metavar = "CSV",
                   dest = "foutp", help = "name file output - required")
-
+parser.add_option("-p", "--plot", type = "str",
+                  dest = "plot", help = "plot clusters (True/False), default = False", default=False)
 (options, args) = parser.parse_args()
 
 if not options.finp:
@@ -45,14 +45,15 @@ if not options.foutp:
 
 if options.nrip == 'None':
     options.nrip = None
-else:
+elif options.nrip:
     try:
         options.nrip = int(options.nrip)
     except ValueError:
         parser.error("option -i: invalid value")
+
 if options.seed == 'None':
     options.seed = None
-else:
+elif options.seed:
     try:
         options.seed = int(options.seed)
     except ValueError:
@@ -76,24 +77,47 @@ m = kmedoid.Medoid(options.nrip,
                    options.radius,
                    options.seed,
                    options.error)
-mat = mat.T
-centroidsid, mini = m.compute(options.k, mat)
+matt = mat.T
+centroidsid, mini = m.compute(options.k, matt)
 
-centroid_matrix = zeros((mat.shape[0], centroidsid.shape[0]))
-for i in range(centroidsid.shape[0]):
-	centroid_matrix[:, i] = mat[:, centroidsid[i]-1]
+if options.plot == "True":
+	import matplotlib
+	matplotlib.use("Agg")
+	import matplotlib.pyplot as plt
+	clusters = {}
+	i = 0
+	for c in mini:
+		try:
+			clusters[c].append(mat[i])
+		except KeyError:
+			clusters[c] = [mat[i]]
+		i += 1
+	colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+	i = 0
+	for cluster in clusters.values():
+		i = i % len(colours)
+		for time_series in cluster:
+			line = plt.plot(time_series)
+			plt.setp(line, color=colours[i])
+		i += 1
+	plt.ylabel(title)
+	plt.savefig(options.foutp, format="png")
+else:
+	centroid_matrix = zeros((matt.shape[0], centroidsid.shape[0]))
+	for i in range(centroidsid.shape[0]):
+		centroid_matrix[:, i] = matt[:, centroidsid[i]-1]
 
-f=open(options.foutp,"w")
-writer = csv.writer(f, delimiter='\t', lineterminator='\n')
+	f=open(options.foutp,"w")
+	writer = csv.writer(f, delimiter='\t', lineterminator='\n')
 
-f.write("Call: iteration %s, distance %s, fast %s, radius %d, seed %s, tollerance %f\n" % (options.nrip,options.met, options.fast,options.radius,options.seed,options.error))
-f.write("centroid.matrix\n")
-writer.writerows(centroid_matrix)
+	f.write("Call: iteration %s, distance %s, fast %s, radius %d, seed %s, tollerance %f\n" % (options.nrip,options.met, options.fast,options.radius,options.seed,options.error))
+	f.write("centroid.matrix\n")
+	writer.writerows(centroid_matrix)
 
-f.write("\ncentroid.idx\n")
-writer.writerow(centroidsid)
-f.write("\ngroups\n")
+	f.write("\ncentroid.idx\n")
+	writer.writerow(centroidsid)
+	f.write("\ngroups\n")
 
-for i in range(mini.shape[0]):
-	writer.writerow([i+1, int(mini[i])])
-f.close()
+	for i in range(mini.shape[0]):
+		writer.writerow([i+1, int(mini[i])])
+	f.close()
