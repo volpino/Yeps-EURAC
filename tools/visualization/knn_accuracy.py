a5f86616665aea369fe782f4d1bb6736b3c6187d
fox <fox91@anche.no>
2010-01-15 18:48:58 +0100

    added % accuracy

--- tools/visualization/knn_accuracy.py
+++ tools/visualization/knn_accuracy.py
@@ -42,7 +42,9 @@ Lenght of the train set:  %d
 Lenght of the test set:   %d
 Right classifications:    %d
 Wrong classifications:    %d
-""" % (len(train), right + wrong, right, wrong)
+Accuracy:                 %d%
+""" % (len(train), right + wrong, right, wrong,
+       100 - float(right) / wrong * 100)
 
 out = open(output, 'w')
 out.write(report)
