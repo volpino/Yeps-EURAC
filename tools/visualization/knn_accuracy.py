f7bee07b965830d132eebb2d52aa0c3120ce619c
fox <fox91@anche.no>
2010-01-15 18:21:20 +0100

    knn accuracy report

---
+++ tools/visualization/knn_accuracy.py
@@ -0,0 +1,36 @@
+from numpy import *
+import csv
+from sys import argv
+
+try:
+    train_set = argv[1]
+    train_labels = argv[2]
+    test_set = argv[3]
+    test_labels = argv[4]
+    output = argv[5]
+except IndexError:
+    print "Invalid input!"
+    exit(1)
+
+train_reader = csv.reader(open(train_set), delimiter='\t')
+train = [row for row in train_labels_reader]
+train_labels_reader = csv.reader(open(train_labels), delimiter='\t')
+train_labels = [row for row in train_labels_reader]
+
+test_reader = csv.reader(open(test_set), delimiter='\t')
+test = [row for row in test_reader]
+test_labels_reader = csv.reader(open(test_labels), delimiter='\t')
+test_labels = [row for row in test_labels_reader]
+
+for i, elem in enumerate(test):
+    try:
+        n = train.index(elem)
+    except ValueError:
+        pass
+    else:
+        for label in train_labels[n]:
+            if label in test_labels[i]:
+                right += 1
+            else:
+                wrong += 1
+print right, wrong, right+wrong
